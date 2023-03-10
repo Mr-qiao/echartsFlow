7e384a8c96843d493c18437885516af9b9b23bce
桐生 <augusyoung@tongshengdeMacBook-Pro.local>
2023-03-10 15:32:27 +0800

    feat: 工厂端一期开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: require.resolve('@umijs/max/eslint'),
+  rules: {
+    // "semi": [2, "always"],//语句强制分号结尾
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
 };
