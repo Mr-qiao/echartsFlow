156690cdd9aefbdd7d04498f43d22979363a1581
桐生 <augusyoung@tongshengdeMacBook-Pro.local>
2023-03-22 19:28:46 +0800

    feat: 联调

--- .umirc.ts
+++ .umirc.ts
@@ -1,28 +1,31 @@
-import { defineConfig } from '@umijs/max';
-import { routes } from './src/routes';
+import {defineConfig} from '@umijs/max';
+import {routes} from './src/routes';
 
 export default defineConfig({
-  model: {},
-  initialState: {},
-  devtool: 'source-map',
-  request: {},
-  locale: {
-    default: 'zh-CN', // 工程默认语言
-    antd: true,
-    // 默认为true。为true时，会使用`navigator.language`覆盖默认。为false时，则使用默认语言
-    baseNavigator: false,
-  },
-  layout: {
-    title: '设计师中台-工厂端',
-  },
-  // theme: { '@primary-color': '#86909C' },
-  routes,
-  npmClient: 'yarn',
-  // proxy: {
-  //   '/api': {
-  //     'target': 'http://192.168.12.124:8080',
-  //     'changeOrigin': true,
-  //     'pathRewrite': { '^/api' : '' },
-  //   }
-  // }
+	model: {},
+	initialState: {},
+	devtool: 'source-map',
+	theme: {
+		"primary-color": "#f5222d",
+	},
+	request: {},
+	locale: {
+		default: 'zh-CN', // 工程默认语言
+		antd: true,
+		// 默认为true。为true时，会使用`navigator.language`覆盖默认。为false时，则使用默认语言
+		baseNavigator: false,
+	},
+	layout: {
+		title: '设计师中台-工厂端',
+	},
+	// theme: { '@primary-color': '#86909C' },
+	routes,
+	npmClient: 'yarn',
+	// proxy: {
+	//   '/api': {
+	//     'target': 'http://192.168.12.124:8080',
+	//     'changeOrigin': true,
+	//     'pathRewrite': { '^/api' : '' },
+	//   }
+	// }
 });
