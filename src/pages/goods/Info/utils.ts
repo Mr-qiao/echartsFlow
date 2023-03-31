793f3a05582c552ea7b6ab0cf866ed0038ec66fc
桐生 <augusyoung@tongshengdeMacBook-Pro.local>
2023-03-31 10:07:22 +0800

    fix: 商品字段修改

--- src/pages/goods/Info/utils.ts
+++ src/pages/goods/Info/utils.ts
@@ -1,17 +1,21 @@
-import { transformFen2Yuan } from '@/utils';
+import {transformFen2Yuan} from '@/utils';
 
 export const formatPriceRange = (priceRange: { left: number; right: number }) => {
-  const { left, right } = transformFen2Yuan(priceRange, ['left', 'right']);
-  if (left === undefined && right !== undefined) {
-    return right ? `${right}元` : '-';
-  }
-  return left && right ? [left, right].join('-') + '元' : '-';
+	const {left, right} = transformFen2Yuan(priceRange, ['left', 'right']);
+	if (left === undefined && right !== undefined) {
+		return right ? `${right}元` : '-';
+	}
+	return left && right ? [left, right].join('-') + '元' : '-';
 };
 
 export const formatRatioRange = (priceRange: { left: number; right: number }) => {
-  const { left, right } = transformFen2Yuan(priceRange, ['left', 'right'], false, 100);
-  if (left === undefined && right !== undefined) {
-    return right ? `${right}%` : '-';
-  }
-  return left && right ? [left, right].join('-') + '%' : '-';
+	const {left: left1, right: right1} = priceRange
+	if (right1 === 0) {
+		return `${0}%`
+	}
+	const {left, right} = transformFen2Yuan(priceRange, ['left', 'right'], false, 100);
+	if (left === undefined && right !== undefined) {
+		return right === 0 || right ? `${right}%` : '-';
+	}
+	return left && right ? [left, right].join('-') + '%' : '-';
 };
