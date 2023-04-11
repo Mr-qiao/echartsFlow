cea0d07d02c65cc1232888b5d78de5ebd92f5afc
顾孝标 <1801688332@qq.com>
2023-04-11 09:40:45 +0800

    fix: branch change

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,32 +3,31 @@ const host = location.host;
 let env = 'production';
 
 if (host.includes('dev') || host.includes('localhost')) {
-	// env = 'development';
-	env = 'daily'
+  env = 'development';
+  // env = 'daily'
 } else if (host.includes('daily')) {
-	env = 'daily';
+  env = 'daily';
 } else if (host.includes('gray')) {
-	env = 'gray';
+  env = 'gray';
 }
 // env = 'daily';
 
-
 const ajaxBaseUrl = {
-	development: 'https://api.dev.xinc818.net',
-	daily: 'https://api.daily.xinc818.net',
-	gray: 'https://api.gray.xinc818.net',
-	production: 'https://api.xinc818.com',
+  development: 'https://api.dev.xinc818.net',
+  daily: 'https://api.daily.xinc818.net',
+  gray: 'https://api.gray.xinc818.net',
+  production: 'https://api.xinc818.com',
 }[env];
 
 const loginUrl: any = {
-	development: 'https://dev.xinc818.net/sso-system/',
-	daily: 'https://daily.xinc818.net/sso-system/',
-	gray: 'https://gray.xinc818.net/sso-system/',
-	production: 'https://gaea.xinc818.com/',
+  development: 'https://dev.xinc818.net/sso-system/',
+  daily: 'https://daily.xinc818.net/sso-system/',
+  gray: 'https://gray.xinc818.net/sso-system/',
+  production: 'https://gaea.xinc818.com/',
 }[env];
 
 export default {
-	ajaxBaseUrl,
-	loginUrl,
-	env,
+  ajaxBaseUrl,
+  loginUrl,
+  env,
 };
