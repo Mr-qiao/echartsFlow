a98826792627c697c9e33f3484620450038c5b3d
顾孝标 <1801688332@qq.com>
2023-04-24 16:40:16 +0800

    fix: 抽离业务视图组件、删除冗于代码、优化代码结构

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -255,6 +255,37 @@ export function transformFen2Yuan<T>(
   return newObj as Record<keyof T, number>;
 }
 
+export const formatPriceRange = (priceRange: {
+  left: number;
+  right: number;
+}) => {
+  const { left, right } = transformFen2Yuan(priceRange, ['left', 'right']);
+  if (left === undefined && right !== undefined) {
+    return right ? `${right}元` : '-';
+  }
+  return left && right ? [left, right].join('-') + '元' : '-';
+};
+
+export const formatRatioRange = (priceRange: {
+  left: number;
+  right: number;
+}) => {
+  const { left: left1, right: right1 } = priceRange;
+  if (right1 === 0) {
+    return `${0}%`;
+  }
+  const { left, right } = transformFen2Yuan(
+    priceRange,
+    ['left', 'right'],
+    false,
+    100,
+  );
+  if (left === undefined && right !== undefined) {
+    return right === 0 || right ? `${right}%` : '-';
+  }
+  return left && right ? [left, right].join('-') + '%' : '-';
+};
+
 /**
  * 延迟一定时间
  * @param {number} time - 延迟时间毫秒数
