8e9c3a828c8f115320bc28eb07c053e34f7f89de
pgl <1801688332@qq.com>
2023-05-09 11:10:58 +0800

    feat: 商品类目优化检索

--- src/components/selectTree/index.tsx
+++ src/components/selectTree/index.tsx
@@ -1,26 +1,34 @@
-import {Cascader} from 'antd';
+import { Cascader } from 'antd';
+
+import type { DefaultOptionType } from 'antd/es/cascader';
 
 function SelectTree(props: any) {
-	const {options = [], onChange, value, ...prop} = props;
-	const onChanges = (value) => {
-		if (value) {
-			onChange(value.length > 0 ? value[value.length - 1] : '')
-		} else {
-			onChange('')
-		}
-	}
-	return (
-		<div>
-			<Cascader
-				{...prop}
-				changeOnSelect
-				// value={value}
-				options={options}
-				onChange={onChanges}
-				placeholder="请选择"
-			/>
-		</div>
-	);
+  const { options = [], onChange, value, ...prop } = props;
+  const onChanges = (value) => {
+    if (value) {
+      onChange(value.length > 0 ? value[value.length - 1] : '');
+    } else {
+      onChange('');
+    }
+  };
+  return (
+    <div>
+      <Cascader
+        {...prop}
+        changeOnSelect
+        // value={value}
+        showSearch={(inputValue: string, path: DefaultOptionType[]) => {
+          return path.some(
+            (option) =>
+              option.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1,
+          );
+        }}
+        options={options}
+        onChange={onChanges}
+        placeholder="请选择"
+      />
+    </div>
+  );
 }
 
 export default SelectTree;
