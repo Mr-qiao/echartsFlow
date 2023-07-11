d08788b9a851c6640c601c9ae42732468e2c5301
guxiaobiao <guxiaobiao@xinxuan.net>
2023-07-11 11:24:39 +0800

    feat: update kl_request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -17,21 +17,42 @@ export const request = {
   },
 };
 
-export const httpRequest = {
-  get(url: string, params?: Recordable<any>, options?: RequestOptions) {
-    return axios(url, {
-      ...options,
-      hasGateway: true,
-      params,
-      method: 'GET',
-    });
+// export const httpRequest = {
+//   get(url: string, params?: Recordable<any>, options?: RequestOptions) {
+//     return axios(url, {
+//       ...options,
+//       hasGateway: true,
+//       params,
+//       method: 'GET',
+//     });
+//   },
+//   post(url: string, data?: Recordable<any>, options?: RequestOptions) {
+//     return axios(url, {
+//       ...options,
+//       hasGateway: true,
+//       data,
+//       method: 'POST',
+//     });
+//   },
+// };
+
+import { navigateToLogin } from '@/utils';
+import type { KunlunResponseProps } from '@xc/kunlun-request';
+import { WebRequest } from '@xc/kunlun-request';
+import { message } from 'antd';
+
+export const httpRequest = new WebRequest({
+  interceptors: {
+    responseInterceptor(response: any) {
+      const { data } = response as KunlunResponseProps;
+      if (data.code === 401) {
+        message.error(data.message);
+      }
+
+      if (data.code === 1000010001 || data.code === 1000010031) {
+        navigateToLogin();
+      }
+      return response;
+    },
   },
-  post(url: string, data?: Recordable<any>, options?: RequestOptions) {
-    return axios(url, {
-      ...options,
-      hasGateway: true,
-      data,
-      method: 'POST',
-    });
-  },
-};
+});
