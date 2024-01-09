f70b6b3de52925145b7bce29ba51760ccbd7d74b
guxiaobiao <guxiaobiao@xinxuan.net>
2024-01-09 22:44:04 +0800

    fix: change

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,34 @@
-import { useModel } from '@umijs/max';
-// import styles from './index.less';
+import ReactEcharts from 'echarts-for-react';
+import React from 'react';
 
 const HomePage: React.FC = () => {
-  const { name } = useModel('global');
-  return <div>先休息休息休息</div>;
+  let option = {
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 2, name: '报警' },
+          { value: 156, name: '正常' },
+          { value: 145, name: '离线' },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        },
+      },
+    ],
+  };
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactEcharts option={option} />
+    </div>
+  );
 };
 
 export default HomePage;
