2dc8c01504e694005ce06d62354b263c3b29fa4d
顾孝标 <guxiaobiao@xinxuan.net>
2024-01-11 13:52:34 +0800

    fix: 优化可视化样式

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -57,134 +57,138 @@ const Home: any = () => {
             <option>凉山州</option>
           </select>
 
-          <div className={styles.floatBox}>
+          <div className={styles.card}>
             <div className={styles.barsBox}>
               <span className={styles.tit}>火灾预警</span>
               <ReactEcharts
                 option={option}
-                style={{ width: 180, height: 160 }}
+                style={{ width: 180, height: 140 }}
               />
             </div>
-            <table className={styles.t_table}>
-              <thead>
-                <tr>
-                  <th>区域</th>
-                  <th>上报时间</th>
-                  <th>跟进人</th>
-                  <th>处理状态</th>
-                  <th>操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-              </tbody>
-            </table>
+            <div className={styles.table_box}>
+              <table className={styles.t_table}>
+                <thead>
+                  <tr>
+                    <th>区域</th>
+                    <th>上报时间</th>
+                    <th>跟进人</th>
+                    <th>处理状态</th>
+                    <th>操作</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
 
-          <div className={styles.floatBox}>
+          <div className={styles.card}>
             <div className={styles.barsBox}>
-              <span className={styles.tit}>火灾预警</span>
+              <span className={styles.tit}>园区巡警</span>
               <ReactEcharts
                 option={option}
-                style={{ width: 180, height: 160 }}
+                style={{ width: 180, height: 140 }}
               />
             </div>
-            <table className={styles.t_table}>
-              <thead>
-                <tr>
-                  <th>区域</th>
-                  <th>上报时间</th>
-                  <th>跟进人</th>
-                  <th>处理状态</th>
-                  <th>操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-              </tbody>
-            </table>
+            <div className={styles.table_box}>
+              <table className={styles.t_table}>
+                <thead>
+                  <tr>
+                    <th>区域</th>
+                    <th>上报时间</th>
+                    <th>跟进人</th>
+                    <th>处理状态</th>
+                    <th>操作</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
         <div className={styles.content_main}>
@@ -214,133 +218,137 @@ const Home: any = () => {
         <div className={styles.content_r}>
           <span className={styles.content_r_time}>统计截止时间：2023-9-20</span>
 
-          <div className={styles.floatBox}>
+          <div className={styles.card}>
             <div className={styles.barsBox}>
-              <span className={styles.tit}>火灾预警</span>
+              <span className={styles.tit}>安全生产</span>
               <ReactEcharts
                 option={option}
-                style={{ width: 180, height: 160 }}
+                style={{ width: 180, height: 140 }}
               />
             </div>
-            <table className={styles.t_table}>
-              <thead>
-                <tr>
-                  <th>区域</th>
-                  <th>上报时间</th>
-                  <th>跟进人</th>
-                  <th>处理状态</th>
-                  <th>操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-              </tbody>
-            </table>
+            <div className={styles.table_box}>
+              <table className={styles.t_table}>
+                <thead>
+                  <tr>
+                    <th>区域</th>
+                    <th>上报时间</th>
+                    <th>跟进人</th>
+                    <th>处理状态</th>
+                    <th>操作</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div className={styles.floatBox}>
+          <div className={styles.card}>
             <div className={styles.barsBox}>
-              <span className={styles.tit}>火灾预警</span>
+              <span className={styles.tit}>事件总量</span>
               <ReactEcharts
                 option={option}
-                style={{ width: 180, height: 160 }}
+                style={{ width: 180, height: 140 }}
               />
             </div>
-            <table className={styles.t_table}>
-              <thead>
-                <tr>
-                  <th>区域</th>
-                  <th>上报时间</th>
-                  <th>跟进人</th>
-                  <th>处理状态</th>
-                  <th>操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-                <tr>
-                  <td>A区</td>
-                  <td>09-21 19:30</td>
-                  <td>王大锤</td>
-                  <td>待处理</td>
-                  <td>详情</td>
-                </tr>
-              </tbody>
-            </table>
+            <div className={styles.table_box}>
+              <table className={styles.t_table}>
+                <thead>
+                  <tr>
+                    <th>区域</th>
+                    <th>上报时间</th>
+                    <th>跟进人</th>
+                    <th>处理状态</th>
+                    <th>操作</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                  <tr>
+                    <td>A区</td>
+                    <td>09-21 19:30</td>
+                    <td>王大锤</td>
+                    <td>待处理</td>
+                    <td>详情</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
