8d1388d9cdacf7053c649d812405e18f46f55791
guxiaobiao <guxiaobiao@xinxuan.net>
2024-03-12 21:58:58 +0800

    fix: 接口对接完成

--- src/components/AvatarName/resetPassword/index.tsx
+++ src/components/AvatarName/resetPassword/index.tsx
@@ -2,13 +2,11 @@
  * 添加用户/编辑用户
  */
 import React from 'react';
-import { Modal, Form, Input, ModalProps } from 'antd';
-import {
-  EyeInvisibleOutlined,
-  EyeTwoTone,
-  LockOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import { Modal, Form, Input, ModalProps, message } from 'antd';
+
+
+import { changePasswd } from '@/services/login'
+
 type AddModalIProps = ModalProps & {
   record?: Recordable<string | undefined>;
 };
@@ -22,61 +20,57 @@ const RestPassword: React.FC<AddModalIProps> = ({ onOk, record, ...restProps }) 
   const handleOk = async (e: any) => {
     try {
       const values = await form.validateFields();
+      await changePasswd({ passwd: values?.passwd })
+      message.success('更新密码成功')
       await onOk?.(values);
     } catch (err) {
       console.log(err);
     }
   };
 
+  const compareToFirstPassword = (rule, value, callback) => {
+    if (value && value !== form.getFieldValue('password')) {
+      return Promise.reject('两次密码必须一致');
+    } else if (!value) {
+      return Promise.reject('请再次输入密码')
+    } else {
+      return Promise.resolve();
+    }
+  };
+
 
   return (
     <Modal title="修改密码" {...restProps} onOk={handleOk}>
-      <Form form={form} labelCol={{ span: 3 }} wrapperCol={{ span: 16 }}>
+      <Form form={form} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }}>
         <Form.Item
           label="密码"
           name="password"
+          hasFeedback
+          extra="密码至少由4个字符组成，必须包含数字、字母，区分大小写"
           rules={[
             {
               required: true,
-              message: '请输入密码',
-              pattern: new RegExp(/^[A-Za-z0-9]+$/, 'g'),
-            },
+              pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{4,16}$/g,
+              message: '密码至少由8个字符组成，必须包含数字、大小写字母，区分大小写'
+            }
           ]}
         >
-          <Input.Password
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            size={'large'}
-            placeholder="请输入密码"
-            bordered
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-            maxLength={20}
-          />
+          <Input.Password />
         </Form.Item>
 
         <Form.Item
           label="新密码"
           name="passwd"
+          hasFeedback
+          extra="请再次输入密码"
           rules={[
             {
               required: true,
-              message: '请输入新密码',
-            },
+              validator: compareToFirstPassword,
+            }
           ]}
         >
-          <Input.Password
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            size={'large'}
-            placeholder="请输入密码"
-            bordered
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-            maxLength={20}
-          />
+          <Input.Password />
         </Form.Item>
 
       </Form>
